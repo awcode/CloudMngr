ecd25a275a6aa3b129f8c256988a8876edc36113
Mark Walker <m@awcode.com>
2014-10-23 16:46:40 +0700

    Start to modularise

---
+++ lib/CloudMngr/cloudmngr.basemodule.class.php
@@ -0,0 +1,16 @@
+<?php
+/* Copyright Mark Walker (AWcode) 2014
+ *
+ * CloudMngrBaseModule Class
+ */
+
+class CloudBaseModule extends CloudMngr{
+	private $module_type;
+
+	function __construct($group_id="", $region_id=""){
+		parent::__construct($group_id, $region_id);
+	}
+
+	
+
+}
