48f1a62291f1c2e02468f5a955da82d8307876db
mnwalker <mark@awcode.com>
2014-10-24 14:48:14 +0700

    Fix Style

--- public_html/index.php
+++ public_html/index.php
@@ -24,6 +24,7 @@ else{$page="main";}
     <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
     <link href="assets/styles.css" rel="stylesheet" media="screen">
     <link href="vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+    <link href="styles.css" rel="stylesheet" media="screen">
      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
