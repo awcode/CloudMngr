d733a8d575fc4c0f58f3ec6467f04f77848a302f
mnwalker <mark@awcode.com>
2014-10-24 13:26:26 +0700

    Fix cmd path

--- public_html/cmd.php
+++ public_html/cmd.php
@@ -12,10 +12,9 @@ $CloudMngr = new CloudMngr($_GET['id']);
 
 $module = $_POST['module'];//{TODO input filertering
 $action = $_POST['action'];
-echo($CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $module.'.php');
-echo $CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "cmd" . DIRECTORY_SEPARATOR . $action .'.php';
+
 if($module != "" && file_exists($CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $module.'.php')){
-	$file = $CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "cmd" . DIRECTORY_SEPARATOR . $action .'.php';
+	$action_file = $CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "cmd" . DIRECTORY_SEPARATOR . $action .'.php';
 	if(file_exists($action_file)){
 		include($action_file);
 	}
