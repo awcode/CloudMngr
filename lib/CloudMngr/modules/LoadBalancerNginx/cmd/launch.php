eaff8d02de81cb23cd4120e2924802e37bf3e639
mnwalker <mark@awcode.com>
2014-10-24 13:30:37 +0700

    Debug cmd

--- lib/CloudMngr/modules/LoadBalancerNginx/cmd/launch.php
+++ lib/CloudMngr/modules/LoadBalancerNginx/cmd/launch.php
@@ -1,8 +1,7 @@
-.<?
+<?
 if($_SESSION['admin'] == ""){die();}
-echo("1");
+
 if(($_POST['region'] != "") && ($_POST['group'] != "")){
-echo("2");
 	$CloudMngr->setGroup($_POST['group']);
 	$CloudMngr->setRegion($_POST['region']);
 
