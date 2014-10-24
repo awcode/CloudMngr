6bd356d319a772294a159a40c20eaa1af7218c90
mnwalker <mark@awcode.com>
2014-10-24 13:25:35 +0700

    Debug cmd

--- public_html/cmd.php
+++ public_html/cmd.php
@@ -12,7 +12,8 @@ $CloudMngr = new CloudMngr($_GET['id']);
 
 $module = $_POST['module'];//{TODO input filertering
 $action = $_POST['action'];
-
+echo($CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $module.'.php');
+echo $CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "cmd" . DIRECTORY_SEPARATOR . $action .'.php';
 if($module != "" && file_exists($CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $module.'.php')){
 	$file = $CloudMngr->class_path . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "cmd" . DIRECTORY_SEPARATOR . $action .'.php';
 	if(file_exists($action_file)){
